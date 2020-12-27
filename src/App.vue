73081e61f46d9d5d23515443988d45295c267fa1
Chirag Arora <chirag.arora5560@gmail.com>
2020-12-27 21:16:34 +0530

    Added Styles

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="bg-gray-200 flex flex-col items-center justify-center h-screen">
-    <div class="bg-white p-12 rounded-lg">
+    <div class="bg-white p-12 rounded-lg shodow hover:shadow-lg">
       <div class="dateToday flex flex-row w-full">
         <div class="fullDate w-1/2">
           <div class="flex">
@@ -16,20 +16,17 @@
         <div class="day w-1/2 text-right">
           <h4 class="dayName" v-text="dateToday.dayName"></h4>
         </div>
-        
       </div>
     <ul>
       <li v-for="(hero,index) in heroes" :key='index' class='bm-2'>
         <span v-text="hero.name" class="rm-2"></span>
-        <button class="rm-2" @click="editHero(hero.name, index)">Edit Me</button>
-        <button class="rm-2" @click="deleteHero(hero.name)">Delete Me</button>
-        <button @click="setFavHero(hero.name)">Set Fav</button>
+        <button class="rm-2 btn-success hover:text-white hover:bg-green-500" @click="setFavHero(hero.name)">Set Fav</button>
+        <button class="rm-2 btn-warning hover:text-white hover:bg-yellow-500" @click="editHero(hero.name, index)">Edit Me</button>
+        <button class="btn-danger hover:text-white hover:bg-red-500" @click="deleteHero(hero.name)">Delete Me</button>
       </li>
     </ul>
-    <input type="text" ref='heroInput' class='rm-2' />
-    <button v-if="!updateHero" @click='addHero'>Set New Hero</button>
-    <button v-else @click='updateHeroMethod'>Update Hero</button>
-    <div v-if="favHeroes.length > 0">
+    <add-new-hero ref='addNewHero' hero='heroes' isUpdate='updateHero' @updateHero=updateHeroMethod @addNewHero="addHero" ></add-new-hero>
+    <div class="mt-8" v-if="favHeroes.length > 0">
       <h2>Favourite Heroes</h2>
       <ol>
         <li class='bm-2' v-for='(hero,index) in favHeroes' :key='index' v-text="hero.name"></li>
@@ -42,10 +39,12 @@
 
 
 <script>
+import AddNewHero from './components/AddNewHero'
 export default {
   name: 'App',
   components: {
-   
+   AddNewHero,
+
   },
   data:function(){
     return {
@@ -56,7 +55,7 @@ export default {
         year:new Date().getFullYear()
       },
       updateHero:false,
-      heroToUpdate:0,
+      
       favHeroes: [],
       heroes:[
         {
@@ -79,37 +78,19 @@ export default {
     }
   },
   methods:{
-    addHero(){
-      
-      //Basic Validation
-      if( this.$refs['heroInput'].value == "" ){
-        this.$refs['heroInput'].focus();
-        return alert("Name Cannot Be Empty");
-        
-      }
-      const name = this.$refs['heroInput'].value;
-      
-      this.$refs['heroInput'].value = '';
-      
-      this.heroes.push( {name} )
-    },
     editHero(heroName,index){
       this.updateHero = true
       this.heroToUpdate = index
-      this.$refs['heroInput'].value = heroName
+      this.$refs['addNewHero'].$refs['heroInput'].value = heroName
     },
+    addHero:function(name){
+      this.heroes.push( { name:name } )
+    },
+    updateHeroMethod:function(heroToUpdate,heroValue){
+      this.heroes[heroToUpdate].name = heroValue
+    }, 
     deleteHero(heroName){
      this.heroes = this.heroes.filter( (hero) => hero.name != heroName ) 
-    },
-    updateHeroMethod(){
-      
-      //Basic Validation
-      if( this.$refs['heroInput'].value == "" ){
-        this.$refs['heroInput'].focus();
-        return alert("Name Cannot Be Empty");
-      }
-
-      this.heroes[this.heroToUpdate].name =  this.$refs['heroInput'].value;
     },
     setFavHero(name){
       this.favHeroes.push( { name:name } );
---
+++ src/components/AddNewHero.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="grid grid-cols-3">
+      <input type="text" ref='heroInput' class='rm-2 col-span-2 input-control'/>
+        <button class="btn-success col-span-1"  v-if="!isUpdate" @click='addHero'>Set New Hero</button>
+        <button class="btn-success col-span-1"  v-else @click='updateHeroMethod'>Update Hero</button>
+    </div>
+</template>
+<script>
+export default{
+        name:'AddNewHeroComponent',
+        data:function(){
+            return{}
+        },
+        props:['heroes','isUpdate','heroToUpdate'],
+        methods:{
+            addHero:function(){
+                //Basic Validation
+                if( this.$refs['heroInput'].value == "" ){
+
+                    this.$refs['heroInput'].focus()
+                    return alert("Name Cannot Be Empty")
+                    
+                }
+                const name = this.$refs['heroInput'].value
+                
+                this.$refs['heroInput'].value = ''
+
+                this.$emit( 'addNewHero', name )
+            },
+            updateHeroMethod:function(){
+                //Basic Validation
+                if( this.$refs['heroInput'].value == "" ){
+                    this.$refs['heroInput'].focus();
+                    return alert("Name Cannot Be Empty");
+                }
+                
+                this.$emit( 'updateHero', this.heroToUpdate,this.$refs['heroInput'].value )
+            }
+        }
+    }
+</script>
