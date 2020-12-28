a45e0b7c3b1187a3eff632738e52d4f329f534f1
Chirag5528 <chirag.arora5560@gmail.com>
2020-12-28 16:02:20 +0530

    Fixed updating issue

--- src/App.vue
+++ src/App.vue
@@ -56,6 +56,7 @@ export default {
         }
       ],
       isDisabled:false,
+      heroToEdit:null,
     }
   },
   methods:{
@@ -63,17 +64,17 @@ export default {
       console.log( hero )
       this.updateHero = true
       this.heroToUpdate = hero.id
+      this.heroToEdit = hero
       this.$refs['addNewHero'].$refs['heroInput'].value = hero.name
     },
     addHero:function(hero){
       this.heroes.push( hero )
     },
     updateHeroMethod:function(){
-
-      this.heroes[this.heroToUpdate - 1].name = this.$refs['addNewHero'].$refs['heroInput'].value
+      const heroIndex = this.heroes.indexOf(this.heroToEdit);
+      this.heroes[heroIndex].name = this.$refs['addNewHero'].$refs['heroInput'].value
       this.updateHero = false
       this.$refs['addNewHero'].$refs['heroInput'].value = ""
-
     }, 
     deleteHero(hero){
      this.heroes = this.heroes.filter( (heroObj) => heroObj.id != hero.id )
