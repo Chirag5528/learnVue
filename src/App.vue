ea70b469df5d5d78fd9654b73418c40abb6cf77a
Chirag5528 <chirag.arora5560@gmail.com>
2021-01-06 12:41:49 +0530

    Just Staging My Changes

--- src/App.vue
+++ src/App.vue
@@ -1,91 +1,34 @@
 <template>
-  <div class="wrapper">
-      <div class="bg-white p-12 rounded-lg shodow hover:shadow-lg">
-        <date-template></date-template>
-        <all-hero-template :heroes="heroes" @setFavHero="setFavHero" @editHero="editHero" @deleteHero="deleteHero" ></all-hero-template>
-      <add-new-hero
-          ref='addNewHero'
-          :isUpdate=updateHero
-          @updateHero="updateHeroMethod"
-          @addNewHero="addHero" />
-        <fav-heroes-component :favHeroes="favHeroes"></fav-heroes-component>
-    </div>
+  <div>
+    <h2>{{ days }}</h2>
   </div>
-    
 </template>
 
 
 <script>
-import AddNewHero from './components/AddNewHero'
-import DateTemplate from './components/DateTemplate'
-import AllHeroTemplate from './components/AllHeroTemplate'
-import FavHeroesComponent from './components/FavHeroesComponent'
+// import DcHeroes from "@/components/DcHeroes/DcHeroes";
 export default {
   name: 'App',
   components: {
-   AddNewHero,
-   DateTemplate,
-   AllHeroTemplate,
-   FavHeroesComponent
-
+    // DcHeroes
   },
-  data:function(){
-    return {
-      updateHero:false,
-      favHeroes: [],
-      heroes:[
-        {
-          id:1,
-          name:'Super Man'
-        },
-        {
-          id:2,
-          name:'Bat Man'
-        },
-        {
-          id:3,
-          name:'Iron Man'
-        },
-        {
-          id:4,
-          name:'Spider Man'
-        },
-        {
-          id:5,
-          name:'Angry Man'
-        }
-      ],
-      isDisabled:false,
-      heroToEdit:null,
+  data(){
+    return{
+
     }
   },
   methods:{
-    editHero(hero){
-      console.log( hero )
-      this.updateHero = true
-      this.heroToUpdate = hero.id
-      this.heroToEdit = hero
-      this.$refs['addNewHero'].$refs['heroInput'].value = hero.name
-    },
-    addHero:function(hero){
-      this.heroes.push( hero )
-    },
-    updateHeroMethod:function(){
-      const heroIndex = this.heroes.indexOf(this.heroToEdit);
-      this.heroes[heroIndex].name = this.$refs['addNewHero'].$refs['heroInput'].value
-      this.updateHero = false
-      this.$refs['addNewHero'].$refs['heroInput'].value = ""
-    }, 
-    deleteHero(hero){
-     this.heroes = this.heroes.filter( (heroObj) => heroObj.id != hero.id )
-    },
-    setFavHero(hero){
-      const contains = this.favHeroes.find(  heroObj =>  heroObj.id == hero.id  );
-      if( !contains ){
-        this.favHeroes.push( hero )
+  },
+  computed:{
+    days:function(){
+      const days = [];
+      let i =0;
+      const d = new Date();
+      for( i = 0; i < 7; i++ ){
+        days.push( new Date(d.getFullYear(),d.getMonth(), d.getDate() + i ).toLocaleString( 'default', { weekday: 'short' } )  )
       }
-
-    },
+      return days
+    }
   }
 }
 </script>
